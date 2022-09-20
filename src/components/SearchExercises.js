fa9ce0b590cf7b2885473faa578ed806477a7b0e
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-21 00:15:04 +0530

    searchExercises

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,59 @@
-import React from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 const SearchExercises = () => {
-  return <div></div>;
+  const [search, setSearch] = useState("");
+  return (
+    <Stack alignContent={"center"} mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: "44px", xs: "30px" },
+        }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises You <br />
+        Should Know
+      </Typography>
+      <Box position="relative" mb="72px" m="auto">
+        <TextField
+          sx={{
+            input: {
+              fontWeight: "700",
+              border: "none",
+              borderRadius: "4px",
+            },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
+            // marginLeft: "200px",
+          }}
+          height="76px"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value.toLowerCase());
+          }}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#ff2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+          }}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
