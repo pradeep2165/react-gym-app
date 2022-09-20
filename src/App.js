26e852f6decb1890e960505c58ecffa1650adbe5
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-20 17:20:03 +0530

    Hero banner content added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ExerciseDetail from "./pages/ExerciseDetail";
 import Footer from "./components/Footer";
+import "./App.css";
 
 const App = () => {
   return (
