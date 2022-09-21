59607b6663b400beab80046f25166f575eb6898c
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-21 16:52:52 +0530

    Exercise are fetched as per category clicked

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -7,15 +7,28 @@ import { exerciseOptions, fetchData } from "../utils/fetchData";
 const Exercises = ({ bodyPart, setExercises, exercises }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const exercisesPerPage = 9;
-  const paginate = (e, value) => {
-    setCurrentPage(value);
-    window.scrollTo({ top: 1800, behavior: "smooth" });
-  };
 
   const indexOfLastExercises = currentPage * exercisesPerPage;
   const indexOfFirstExercises = indexOfLastExercises - exercisesPerPage;
   const currentExercise = exercises.slice(indexOfFirstExercises, indexOfLastExercises);
 
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exerciseData = [];
+      if (bodyPart === "all") {
+        exerciseData = await fetchData("https://exercisedb.p.rapidapi.com/exercises", exerciseOptions);
+      } else {
+        exerciseData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+      }
+      setExercises(exerciseData);
+    };
+    fetchExercisesData();
+  }, [bodyPart]);
   return (
     <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
       <Typography variant="h3" mb="46px">
@@ -33,7 +46,7 @@ const Exercises = ({ bodyPart, setExercises, exercises }) => {
           <ExerciseCard key={index} exercise={exercise} />
         ))}
       </Stack>
-      <Stack mt="100px" alignItem="center">
+      <Stack mt="100px" alignItems="center">
         {exercises.length > 9 && <Pagination color="standard" shop="rouned" defaultPage={1} count={Math.ceil(exercises.length / exercisesPerPage)} page={currentPage} onChange={paginate} size="large" />}
       </Stack>
     </Box>
