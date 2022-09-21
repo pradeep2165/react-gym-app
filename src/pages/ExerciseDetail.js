c3c14d25136878c5119450223975fac721968d95
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-21 18:16:42 +0530

    Details component completed

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -5,12 +5,27 @@ import { exerciseOptions, fetchData } from "../utils/fetchData";
 import Details from "../components/Details";
 import ExerciseVideo from "../components/ExerciseVideo";
 import SimilarExercises from "../components/SimilarExercises";
+
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl = "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      console.log(exerciseDetailData);
+      setExerciseDetail(exerciseDetailData);
+    };
+    fetchExercisesData();
+  }, [id]);
   return (
     <Box>
-      <Details />
-      <ExerciseVideo />
-      <SimilarExercises />
+      <Details exerciseDetail={exerciseDetail} />
+      {/* <ExerciseVideo />
+      <SimilarExercises /> */}
     </Box>
   );
 };
