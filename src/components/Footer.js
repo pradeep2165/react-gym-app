f979a79d19658e19a82337add7d95fa56d62b927
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-21 22:27:40 +0530

    footer component completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
-
+import Logo from "../assets/assets/images/Logo-1.png";
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="48px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h5" pb="40px" mt="40px">
+          Made by pradeep With help of JM
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
