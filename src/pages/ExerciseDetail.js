77a6662756e1a2660d934a804d76c8a67ce2d214
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-21 17:03:15 +0530

    details, exerciseVideo and SimilarExercises component created

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,7 +1,18 @@
-import React from "react";
-
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import Details from "../components/Details";
+import ExerciseVideo from "../components/ExerciseVideo";
+import SimilarExercises from "../components/SimilarExercises";
 const ExerciseDetail = () => {
-  return <div>ExerciseDetail</div>;
+  return (
+    <Box>
+      <Details />
+      <ExerciseVideo />
+      <SimilarExercises />
+    </Box>
+  );
 };
 
 export default ExerciseDetail;
